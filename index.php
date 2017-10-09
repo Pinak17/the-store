c0d2ddec44e25ca7cec2bd4401a8a2da230648cf
Pinak17 <pinak.roychowdhury@gmail.com>
2017-10-09 16:26:41 +0530

    Index modified-Navbar Structure added to new index

--- index.php
+++ index.php
@@ -1,89 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
-<html lang="en">
-<head>
-  <title>The Collective Outlet</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-<!--<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span> 
-      </button>
-      <a class="navbar-brand" href="#">The Collective Outlet</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">T-SHIRTS</a></li>
-        <!--<li><a href="#">Page 2</a></li> 
-        <li><a href="#">Page 3</a></li>
-      </ul>
-      <!--<ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<section>
-<div class="container-fluid jumbotron text-center" style="background-color:lightblue;">
-  <h1>The Collective Outlet</h1>
-  <p>Exclusive designs.</p> 
-</div>
-</section>-->
-<section>
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-  </ol>
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" style="background-color: black;">
-    <div class="item active img-responsive">
-      <img src="assets/images/promoted-product/product1.jpeg" alt="Shirts" style="padding-left:10%;padding-right:10%;width:100%;height:500px">
-      <div class="carousel-caption">
-        <h3>Los Angeles</h3>
-        <p>LA is always so much fun!</p>
-      </div>
-    </div>
-    <div class="item img-responsive">
-      <img src="assets/images/promoted-product/product2.jpeg" alt="Shirts" style="padding-left:10%;padding-right:10%;width:100%;height:500px">
-      <div class="carousel-caption">
-        <h3>Chicago</h3>
-        <p>Thank you, Chicago!</p>
-      </div>
-    </div>
-    <div class="item img-responsive">
-      <img src="assets/images/promoted-product/product3.jpeg" alt="Shirts" style="padding-left:10%;padding-right:10%;width:100%;height:500px">
-      <div class="carousel-caption">
-        <h3>New York</h3>
-        <p>We love the Big Apple!</p>
-      </div>
-    </div>
-  </div>
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-<div class="row">
-	<div class=""
-</section>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>the Store?</title>
+
+		<!-- Bootstrap -->
+		<link href="assets/bstrap/css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+  <body>
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+			<div class="container-fluid">
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				    <span class="icon-bar"></span>
+				    <span class="icon-bar"></span>
+				    <span class="icon-bar"></span>                        
+				  </button>
+				  <a class="navbar-brand" href="#">WebSiteName</a>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+				  <ul class="nav navbar-nav navbar-right">
+				    <li class="active"><a href="#">Home</a></li>
+				    <li class="dropdown">
+				      <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+				      <ul class="dropdown-menu">
+				        <li><a href="#">Page 1-1</a></li>
+				        <li><a href="#">Page 1-2</a></li>
+				        <li><a href="#">Page 1-3</a></li>
+				      </ul>
+				    </li>
+				    <li><a href="#">Page 2</a></li>
+				    <li><a href="#">Page 3</a></li>
+				  </ul>				  
+				</div>
+			</div>
+		</nav>	
+    	
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/bstrap/js/bootstrap.min.js"></script>
+	</body>
 </html>
