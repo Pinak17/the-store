e39683c71b78a41972e6556f0536229e1577ac3e
Pinak17 <pinak.roychowdhury@gmail.com>
2017-10-18 21:51:06 +0530

    insert product into table done.

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -25,7 +25,7 @@ include "includes/db.php";
 	<body>
 		
 	</body>
-		<form action="insertProduct.php" method="post" enctype="multipart/form-data">
+		<form action="insert_product.php" method="post" enctype="multipart/form-data">
 			<table align="center" width="750" border="2" bgcolor="#ccff99">
 				<tr align="center">
 					<td colspan="8"><h2>Insert New Product Here</h2></td>
@@ -74,7 +74,7 @@ include "includes/db.php";
 					<td><input type="text" name="product_keyword" /></td>
 				</tr>
 				<tr align="center">					
-					<td colspan="8"><input type="submit" name="Submit" value="Submit" /></td>
+					<td colspan="8"><input type="submit" name="insert_product" value="Submit" /></td>
 				</tr>
 			</table>
 		</form>
@@ -84,5 +84,28 @@ include "includes/db.php";
 		<script src="assets/bstrap/js/bootstrap.min.js"></script>
 	</body>
 </html>
-	
+<?php
+if(isset($_POST['insert_product'])){
+	$product_title=$_POST['product_title'];
+	$product_cat=$_POST['product_cat'];
+	$product_brand=$_POST['product_brand'];
+	$product_code=$_POST['product_code'];
+	$product_desc=$_POST['product_desc'];
+	$product_price=$_POST['product_price'];
+	$product_keyword=$_POST['product_keyword'];
 
+	$product_image=$_FILES['product_image']['name'];
+	$product_image_tmp=$_FILES['product_image']['tmp_name'];
+
+	move_uploaded_file($product_image_tmp, "product_images/$product_image");
+	
+	$insert_product="INSERT INTO `products` (product_cat,product_brand,product_code,product_title,product_price,product_desc,product_image,product_keyword) VALUES ('$product_cat','$product_brand','$product_code','$product_title','$product_price','$product_desc','$product_image','$product_keyword')";
+
+	$insert_pro=mysqli_query($con, $insert_product);
+	if($insert_pro){
+		echo "<script>alert('Product has been inserted.')</script>";
+		echo "<script>window.open('insert_product.php', '_SELF')</script>";
+
+	}
+}
+?>
