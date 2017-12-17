2dacea921d6345c6b8fbb10afdd5cc92e7b689c0
Pinak17 <pinak.roychowdhury@gmail.com>
2017-12-18 00:26:46 +0530

    Files added

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/the-store/admin/');
+define('HTTP_CATALOG', 'http://localhost/the-store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/the-store/admin/');
+define('HTTPS_CATALOG', 'http://localhost/the-store/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/htdocs/the-store/admin/');
+define('DIR_SYSTEM', '/opt/lampp/htdocs/the-store/system/');
+define('DIR_IMAGE', '/opt/lampp/htdocs/the-store/image/');
+define('DIR_LANGUAGE', '/opt/lampp/htdocs/the-store/admin/language/');
+define('DIR_TEMPLATE', '/opt/lampp/htdocs/the-store/admin/view/template/');
+define('DIR_CONFIG', '/opt/lampp/htdocs/the-store/system/config/');
+define('DIR_CACHE', '/opt/lampp/htdocs/the-store/system/storage/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/htdocs/the-store/system/storage/download/');
+define('DIR_LOGS', '/opt/lampp/htdocs/the-store/system/storage/logs/');
+define('DIR_MODIFICATION', '/opt/lampp/htdocs/the-store/system/storage/modification/');
+define('DIR_UPLOAD', '/opt/lampp/htdocs/the-store/system/storage/upload/');
+define('DIR_CATALOG', '/opt/lampp/htdocs/the-store/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'kart6');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
